YaLTeR/niri

commit 396097c3ab67a5482a9d7614992c2c603c90e313

Fix constant repaint in the open overview

src/layout/mod.rs
@@ -498,6 +498,7 @@ pub enum HitType {
 enum OverviewProgress {
     Animation(Animation),
     Gesture(OverviewGesture),
+    Open,
 }
 
 #[derive(Debug)]
@@ -628,6 +629,7 @@ impl OverviewProgress {
         match self {
             OverviewProgress::Animation(anim) => anim.value(),
             OverviewProgress::Gesture(gesture) => gesture.value,
+            OverviewProgress::Open => 1.,
         }
     }
 
@@ -2648,9 +2650,11 @@ impl<W: LayoutElement> Layout<W> {
             }
         }
 
-        if !self.overview_open {
-            if let Some(OverviewProgress::Animation(anim)) = &mut self.overview_progress {
-                if anim.is_done() {
+        if let Some(OverviewProgress::Animation(anim)) = &mut self.overview_progress {
+            if anim.is_done() {
+                if self.overview_open {
+                    self.overview_progress = Some(OverviewProgress::Open);
+                } else {
                     self.overview_progress = None;
                 }
             }

src/layout/monitor.rs
@@ -282,6 +282,7 @@ impl From<&super::OverviewProgress> for OverviewProgress {
         match value {
             super::OverviewProgress::Animation(anim) => Self::Animation(anim.clone()),
             super::OverviewProgress::Gesture(gesture) => Self::Value(gesture.value),
+            super::OverviewProgress::Open => Self::Value(1.),
         }
     }
 }

jiji-wm/jiji

commit 73cdb2cb66dbf131a1e5808744f0ed83cb415682

Fix missing redraw when floating DnD in overview scrolls workspaces
Regressed in 396097c3ab67a5482a9d7614992c2c603c90e313

src/layout/mod.rs
@@ -2696,7 +2696,7 @@ impl<W: LayoutElement> Layout<W> {
                 }
 
                 // Keep advancing animations if we might need to scroll the view.
-                if !move_.is_floating {
+                if !move_.is_floating || self.overview_open {
                     return true;
                 }
             }